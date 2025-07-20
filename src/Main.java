438a9733d8ce5326f1a72687558d7b50e8cec219
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-07-20 11:38:22 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 class Student {
-     String name;
-     int rollNumber;
-     char grade;
-     public void displayDetails() {
-          System.out.println("Name: "+ name);
-          System.out.println("Roll Number: "+ rollNumber);
-          System.out.println("Grade: "+ grade);
-     }
+    String name;
+    int rollNumber;
+    char grade;
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+    }
 }
+
 public class Main {
-      public static void main(String[] args) {
-        Student student=new Student();
-           student.name= "Ganesh Chandran";
-           student.rollNumber= 36;
-           student.grade= 'S';
-           student.displayDetails();
-    }
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Ganesh Chandran";
+        student.rollNumber = 36;
+        student.grade = 'S';
+        student.displayDetails();
+    }
 }
