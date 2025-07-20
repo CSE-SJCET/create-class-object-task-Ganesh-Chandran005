7fb96820f7933e5776820c963261097413304e5b
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-07-20 11:23:26 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+class Student {
+     String name;
+     int rollNumber;
+     char grade;
+     
+     public void displayDetails() {
+          System.out.println("Name: "+name);
+          System.out.println("Roll Number: "+rollNumber);
+          System.out.println("Grade: "+grade);
+     }
+}
 
-    // TODO: Create a method to display student details
 public class Main {
       public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
-    }
+        Student student=new Student();
+           student.name= "Ganesh Chandran";
+           student.rollNumber= 36;
+           student.grade= 'S';
+
+           student.displayDetails();
+    }
 }
