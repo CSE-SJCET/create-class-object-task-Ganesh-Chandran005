da432edf70dd233c1acb1a1638d01bb71f39b8e8
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-07-20 11:33:52 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ class Student {
      int rollNumber;
      char grade;
      public void displayDetails() {
-          System.out.println("Name:"+ name);
-          System.out.println("Roll Number:"+ rollNumber);
-          System.out.println("Grade:"+ grade);
+          System.out.println("Name: "+ name);
+          System.out.println("Roll Number: "+ rollNumber);
+          System.out.println("Grade: "+ grade);
      }
 }
 public class Main {
